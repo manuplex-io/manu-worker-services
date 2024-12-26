d7ff5466c6a49c1b64ecf106b2ab3469fb70bce2
Apoorv1388 <apoorv@manuplex.io>
2024-12-26 18:53:47 +0530

    Added new Health check method

--- codePipeline/codeDeploy/scripts/health_check.sh
+++ codePipeline/codeDeploy/scripts/health_check.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-# Perform health check by checking if the response contains "status":"ok"
-#!/bin/bash
-
 # Wait for 60 seconds
 sleep 60
-
 # Perform the health check
-if curl -s https://os.manuplex.io/services/health | grep -q '"status":"ok"'; then
+HEALTHCHECK_URL="https://consul.prod.forty-two.xyz/v1/health/checks/worker-services"
+# Fetch the health check response
+response=$(curl -s "$HEALTHCHECK_URL")
+# Check if any service has a "Status": "passing"
+if echo "$response" | grep -q '"Status":"passing"'; then
   echo "Health check passed."
   echo "success" > /tmp/health_check_status  # Write success to a temporary file
   exit 0  # Exit with success
