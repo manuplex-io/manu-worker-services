3fc13d53e5c438d68ff3fcc97648bc04ebd01c61
Apoorv1388 <apoorv@manuplex.io>
2025-01-04 19:21:05 +0530

    Changes

--- codePipeline/codeDeploy/scripts/deployDocker.sh
+++ codePipeline/codeDeploy/scripts/deployDocker.sh
@@ -4,7 +4,7 @@ set -e
 # Customize the stack name here:
 STACK_NAME="${1:-uSt-bS}"
 
-REPLICAS="${2:-2}" # Defaults to 1 if not provided
+REPLICAS="${2:-1}" # Defaults to 1 if not provided
 
 # The script assumes docker-stack.yml is in the same directory if we copied it via appspec.yml:
 cd /home/ec2-user/codeDeploy/scripts
